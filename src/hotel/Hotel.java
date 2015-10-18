c08cad24abc397a2a26f05c4073dbb3e5e82f1c4
Morr <Morr@Morr-Komputer>
2015-10-18 20:14:55 +0200

    Hotel and Room classes

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,11 +1,43 @@
 package hotel;
 
-public class Hotel {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
+public class Hotel {
+    
+    ArrayList<Room> rooms;
         
+    public Hotel() {
+        rooms = new ArrayList<Room>();
+    }
         
+    //Liczba osób aktualnie przyebywających w hotelu
+    public int getLocatorsCount() {
+        int locators = 0;
+        for(Room room: rooms) {
+            Reservation reservation = room.getCurrentReservation();
+            if(reservation != null)
+                locators += reservation.locatorsCount;
+        }
+        return locators;
+    }
+	
+    void addRoom(Room room) {
+        rooms.add(room);
+    }
+	
+    void removeRoom(Room room) {
+        rooms.remove(room);
+    }
+	
+	
+    public static void Main(String[] argv) {
+        Hotel hotel = new Hotel();
         
+        Room newRoom = new Room(1, 4, 100, Room.RoomStandard.HIGH);
+        Reservation newReservation = new Reservation();
+        newReservation.locatorsCount = 3;
+        System.out.println(newRoom.addReservation(newReservation));
         
     }
     
