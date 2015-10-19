a206e6de6f859116f3e9056596284a03871ab194
Kasia <k.bigaj@uj.edu.pl>
2015-10-19 10:39:41 +0200

    Date extended

--- src/hotel/Interval.java
+++ src/hotel/Interval.java
@@ -1,11 +1,28 @@
 package hotel;
 
+import java.util.Date;
+
 public class Interval {
 
-    public boolean collides(Interval other) {
-        return false;
+    private Date begin;
+    private Date end;
 
+    Interval (Date b, Date e)  {
+        begin = b;
+        end = e;
     }
 
+    Date getBegin() {
+        return begin;
+    }
 
+    Date getEnd() {
+        return end;
+    }
+
+    public boolean collides(Interval other) {
+        boolean caseA = end.after(other.getBegin()) && begin.before(other.getEnd());
+        boolean caseB = begin.after(other.getEnd()) && end.before(other.getBegin());
+        return !(caseA || caseB);
+    }
 }
