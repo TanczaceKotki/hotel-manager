b8753be1bb23941d5f820180a4c1bbd9cbdf8eae
Kasia <k.bigaj@uj.edu.pl>
2015-10-26 13:09:02 +0100

    Class Person added

--- src/hotel/Person.java
+++ src/hotel/Person.java
@@ -1,13 +1,64 @@
 package hotel;
 
 public class Person {
-    int id;
-    String firstname;
-    String surname;
-    String telephone;
-    String email;
+    private int id;
+    private String firstname;
+    private String surname;
+    private String telephone;
+    private String email;
 
     public Person(int id, String firstname,
                   String surname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.telephone = "";
+        this.email = "";
+    }
+
+    public Person(int id, String firstname,
+                  String surname, String telephone) {
+        this.id = id;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.telephone = telephone;
+        this.email = "";
+    }
+
+    public Person(int id, String firstname,
+                  String surname, String telephone, String email) {
+        this.id = id;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.telephone = tel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
